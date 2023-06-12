b4279f55f990699c49df5d265e0a38f449564c59
DarioFi <filatrella.dario@gmail.com>
2023-06-12 15:18:28 +0200

    sync

--- run.py
+++ run.py
@@ -169,16 +169,16 @@ def simulate(L, q):
 
     ordered_temps = []
 
-    steps = (1 * 10 ** 7, 10 ** 8, 4 * 10 ** 7)
-    burnin = (1 * 10 ** 7, 10 ** 8, 4 * 10 ** 7)
+    steps = (1 * 10 ** 7, 5 * 10 ** 7, 10 ** 7)
+    burnin = (1 * 10 ** 7, 5 * 10 ** 7, 10 ** 7)
 
     for temps, step, burn in zip(temps_triple, steps, burnin):
         for t in temps:
             tempo = time.time()
             ordered_temps.append(t)
             if abs(t - critical_Temperature(q)) < 0.05:
-                print(f"Starting simulation for {q=} {L=} {t=} with step={5 * 10 ** 8} and burnin={2 * 10 ** 8}")
-                en, mag = MCMC(L, q, t, 5 * 10 ** 8, 2 * 10 ** 8)
+                print(f"Starting simulation for {q=} {L=} {t=} with step={10 ** 8} and burnin={10 ** 8}")
+                en, mag = MCMC(L, q, t, 10 ** 8, 10 ** 8)
             else:
                 print(f"Starting simulation for {q=} {L=} {t=} with step={step} and burnin={burn}")
                 en, mag = MCMC(L, q, t, step, burn)
@@ -272,13 +272,10 @@ import multiprocessing as mp
 # print(get_temps(2))
 
 qs_Ls = [
-    (4, 50),
-    (5, 50),
+    (3, 20),
 ]
 
 if __name__ == '__main__':
-    print(get_temps(4))
-    print(get_temps(5))
     with mp.Pool(1) as p:
         print(p.map(simulate_save, qs_Ls))
     # print(get_temps(8))
